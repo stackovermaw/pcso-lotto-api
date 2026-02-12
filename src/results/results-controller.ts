e09a8f9aee89cc0e298797d070ef9afc1a0eda1f
stackovermaw <johncarlo.camara1@gmail.com>
2026-02-12 09:48:43 +0800

    feat: add get results by date

--- src/results/results-controller.ts
+++ src/results/results-controller.ts
@@ -1,14 +1,35 @@
 import type { Request, Response } from "express";
 
-import { LOCALE, LOCALE_OPTIONS, RESULTS_TODAY_URL } from "./_constants";
+import {
+  LOCALE,
+  LOCALE_OPTIONS,
+  RESULTS_BY_DATE_URL,
+  RESULTS_TODAY_URL,
+} from "./_constants";
 import { parseResults } from "./results-service";
 
-export const getResultsToday = async (_request: Request, res: Response) => {
+export const getResultsToday = async (_req: Request, res: Response) => {
   const data = await parseResults({
     url: RESULTS_TODAY_URL,
   });
   res.status(200).send({
     date: new Date().toLocaleDateString(LOCALE, LOCALE_OPTIONS),
+    ...data,
+  });
+};
+
+export const getResultsByDate = async (req: Request, res: Response) => {
+  const { date } = req.params;
+
+  const url = RESULTS_BY_DATE_URL + date;
+
+  const data = await parseResults({
+    url,
+    filterDate: date,
+  });
+
+  res.status(200).send({
+    date: new Date(date).toLocaleDateString(LOCALE, LOCALE_OPTIONS),
     ...data,
   });
 };
--- src/results/results-route.ts
+++ src/results/results-route.ts
@@ -1,12 +1,13 @@
-import { type NextFunction, Router } from "express";
+import { Router } from "express";
 import * as v from "valibot";
 
 import { resultsController } from "./index";
-
-const _validateBody =
-  (schema: any) => (req: Request, res: Response, next: NextFunction) => {
-    const _out = v.parse(schema, req.body);
-  };
+import {
+  isAFutureDate,
+  isWithinMonthDays,
+  isWithinValidDate,
+} from "./utils/date";
+import { DATE_REGEX, validate } from "./utils/validate";
 
 export const resultsRouter = Router();
 
@@ -23,3 +24,53 @@ export const resultsRouter = Router();
  *
  */
 resultsRouter.get("/today", resultsController.getResultsToday);
+
+/**
+ * @swagger
+ * /api/results/{date}:
+ *   get:
+ *     tags:
+ *      - Results
+ *     description: Get results by date
+ *     parameters:
+ *       - in: path
+ *         name: date
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: april-6-2025
+ *         description: The date for which results are requested
+ *     responses:
+ *       200:
+ *         description: Returns results for the specified date.
+ *       400:
+ *         description: Bad Request - Invalid date format or date out of range.
+ */
+resultsRouter.get(
+  "/:date",
+  validate(
+    v.object({
+      date: v.pipe(
+        v.string(),
+        v.regex(
+          DATE_REGEX,
+          "Date must be in the format 'month-day-year', e.g., 'april-6-2025'",
+        ),
+        v.check(
+          isWithinMonthDays,
+          "Date is not within the valid number of days for the month",
+        ),
+        v.check(
+          isWithinValidDate,
+          "Date must be on or after June 3, 2024, the earliest parsable results date",
+        ),
+        v.check(
+          isAFutureDate,
+          "Date cannot be in the future, we aren't time travelers!",
+        ),
+      ),
+    }),
+    "params",
+  ),
+  resultsController.getResultsByDate,
+);
