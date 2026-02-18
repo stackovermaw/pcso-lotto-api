337cdf199c3dbda2080c4fcf8828e2c6daaa4361
stackovermaw <johncarlo.camara1@gmail.com>
2026-02-18 23:15:21 +0800

    chore: update import paths

--- src/startup/swagger.ts
+++ src/startup/swagger.ts
@@ -1,7 +1,7 @@
 import type { Express } from "express";
 import { serve, setup } from "swagger-ui-express";
 
-import { openapiSpecs } from "../lib/swaggerInit";
+import { openapiSpecs } from "../api/lib/swaggerInit";
 
 export const swaggerSetup = (app: Express) => {
   app.use("/api/docs", serve, setup(openapiSpecs));
