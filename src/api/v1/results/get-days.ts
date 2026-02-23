4e3526049682df113e05c6e3cfa88204a3673284
stackovermaw <johncarlo.camara1@gmail.com>
2026-02-23 21:06:45 +0800

    fix: get days returns the prev month days

--- src/api/v1/results/get-days.ts
+++ src/api/v1/results/get-days.ts
@@ -1,3 +1,5 @@
 export const getDays = (year: number, month: number) => {
-  return new Date(year, month, 0).getDate();
+  // needs to add 1 because having day 0 gets the last day of the previous month,
+  // so month + 1 gets the last day of the current month
+  return new Date(year, month + 1, 0).getDate();
 };
