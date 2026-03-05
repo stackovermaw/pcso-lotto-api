03876153083ceae9ff55a60a25e47f2ba69ad07a
stackovermaw <johncarlo.camara1@gmail.com>
2026-03-05 16:55:25 +0800

    refactor: move dir

--- src/api/v1/results/utils/date.ts
+++ src/api/v1/results/utils/date.ts
@@ -1,3 +1,13 @@
+import { MONTHS } from "../../../results/_constants";
+
+export const formatDate = (date: Date) => {
+  const month = MONTHS[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  return `${month}-${day}-${year}`;
+};
+
 export const getDays = (year: number, month: number) => {
   // needs to add 1 because having day 0 gets the last day of the previous month,
   // so month + 1 gets the last day of the current month
